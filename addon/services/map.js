1ce4ffab171fd01f747b0aa063de4f7cc5bf5708
Marissa Wood <marissa.wood@marianatek.com>
2020-12-07 12:04:04 -0500

    remove extra settings

--- addon/services/map.js
+++ addon/services/map.js
@@ -13,9 +13,7 @@ export default Service.extend({
     L.marker(point).addTo(map);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      tileSize: 512,
       maxZoom: 18,
-      zoomOffset: -1,
       id: 'mapbox/streets-v11',
       accessToken: getOwner(this).resolveRegistration('config:environment')['ember-simple-leaflet-maps'].apiKey
     }).addTo(map);
