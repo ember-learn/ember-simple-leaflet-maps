ed33f02c5481840f963da6f4f7576751e04d9c2b
Marissa Wood <marissa.wood@marianatek.com>
2020-12-07 11:54:16 -0500

    update mapbox url schema for mapbox leaflets

--- addon/services/map.js
+++ addon/services/map.js
@@ -11,10 +11,12 @@ export default Service.extend({
       zoom: 13
     });
     L.marker(point).addTo(map);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      tileSize: 512,
       maxZoom: 18,
-      id: 'mapbox.streets',
+      zoomOffset: -1,
+      id: 'mapbox/streets-v11',
       accessToken: getOwner(this).resolveRegistration('config:environment')['ember-simple-leaflet-maps'].apiKey
     }).addTo(map);
     schedule('afterRender', () => map.invalidateSize());
